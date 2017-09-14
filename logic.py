20fbf664571e29ed5a5a2fdceb395e9ab8ab2496
Marselliy <sariksariksarik@gmail.com>
2017-09-14 23:51:48 +0300

    Log removed

--- logic.py
+++ logic.py
@@ -148,7 +148,6 @@ def merge(mat):
 
 
 def up(game):
-        print("up")
         # return matrix after shifting up
         game=transpose(game)
         game,done=cover_up(game)
@@ -160,7 +159,6 @@ def up(game):
         return (game,done)
 
 def down(game):
-        print("down")
         game=reverse(transpose(game))
         game,done=cover_up(game)
         temp=merge(game)
@@ -171,7 +169,6 @@ def down(game):
         return (game,done)
 
 def left(game):
-        print("left")
         # return matrix after shifting left
         game,done=cover_up(game)
         temp=merge(game)
@@ -181,7 +178,6 @@ def left(game):
         return (game,done)
 
 def right(game):
-        print("right")
         # return matrix after shifting right
         game=reverse(game)
         game,done=cover_up(game)
